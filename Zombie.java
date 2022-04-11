4c827f4efda1af5d8bac44b6580bad1fd7a056b1
Luisa María Álvarez García <luisamariaalvarezgarcia@gmail.com>
2022-04-11 12:56:15 -0500

    segundo commit

--- Zombie.java
+++ Zombie.java
@@ -5,9 +5,67 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.Date;
 
-import java.util.Scanner;
 public class Zombie
 {
+    //atributos
+    private String nombre; 
+    private int salud;
+    private Date date;
+    private String tipoDeSangre;
     
+    // mètodo
+    public Zombie(String nombre, int salud, Date date, String tipoDeSangre)
+    {
+        this.nombre= nombre;
+        this.salud= salud;
+        this.date= date;
+        this.tipoDeSangre= tipoDeSangre;
+    }
+    
+    //setters
+    
+    public void setNombre(String nombre)
+    {
+        this.nombre=nombre;
+    }
+    public void setSalud(int salud)
+    {
+        this.salud=salud;
+    }
+    public void setDate(Date date)
+    {
+        this.date=date;
+    }
+    public void setTipoDeSangre(String tipoDeSangre)
+    {
+        this.tipoDeSangre=tipoDeSangre;
+    }
+    //getter
+    public String getNombre()
+    {
+        return nombre;
+    }
+    public int getSalud()
+    {
+        return salud;
+    }
+    public Date getDate()
+    {
+        return date;
+    }
+    public String getTipoDeSangre()
+    {
+        return tipoDeSangre;
+    }
+    
+    public int imprimir()
+    {
+        System.out.println(1);
+        return 1;
+    }
 }
+    
+
+    
